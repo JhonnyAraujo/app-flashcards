e6abde4e189b40c3c6aea9019528a024afb9c887
Jhonny Araujo <jhonny.a.santana2002@gmail.com>
2026-04-18 14:03:40 -0300

    feat: adiciona a layout estatico da HomePage

--- lib/pages/home.page.dart
+++ lib/pages/home.page.dart
@@ -11,6 +11,38 @@ class HomePage extends StatelessWidget {
         title: const Text("Decks", style: TextStyle(color: Colors.white)),
         centerTitle: true,
       ),
+      body: SafeArea(
+        child: ListView(
+          children: const [
+            ListTile(
+              contentPadding: EdgeInsets.symmetric(vertical: 20),
+              title: Text(
+                "Flutter",
+                style: TextStyle(fontSize: 28),
+                textAlign: TextAlign.center,
+              ),
+              subtitle: Text("0 cartões", textAlign: TextAlign.center),
+              shape: Border(bottom: BorderSide()),
+            ),
+            ListTile(
+              contentPadding: EdgeInsets.symmetric(vertical: 20),
+              title: Text(
+                "NodeJS",
+                style: TextStyle(fontSize: 28),
+                textAlign: TextAlign.center,
+              ),
+              subtitle: Text("0 cartões", textAlign: TextAlign.center),
+              shape: Border(bottom: BorderSide()),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        shape: const StadiumBorder(),
+        backgroundColor: Colors.black,
+        onPressed: () {},
+        label: const Text("Adicionar", style: TextStyle(color: Colors.white)),
+      ),
     );
   }
 }
